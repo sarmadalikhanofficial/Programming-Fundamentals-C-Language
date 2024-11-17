01c1d0c02ddea047dbfd256acd1023b13e4c75f1
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-17 23:15:04 +0500

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+
+{ 
+    int a = 4; // variable 'a' is initialized with the value 4
+    int b; // variable 'b' is declared but not initialized yet
+    b = 21; // variable 'b' is assigned the value 21
+    
+    // print the experience and age using the values of 'a' and 'b'
+    printf("My experience is of %d years and my age is %d", a, b);
+} 
