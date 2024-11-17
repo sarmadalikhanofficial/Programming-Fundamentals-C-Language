91e820a1906bcf5b9e9d251f32377b81f485c4c7
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-17 12:58:21 +0500

    Update heyworld.c

--- heyworld/heyworld.c
+++ heyworld/heyworld.c
@@ -2,7 +2,7 @@
 
 // main function where the program starts execution
 int main() {
-    // print "Hello, World!" to the console
+    // print "Hey, World!" to the console
     printf("Hey, World!\n");
     
     // return 0 to indicate successful execution
