a2366cf3e36178368d9539d7a820cb22f7ab0962
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-18 02:59:27 +0500

    Create gradingSystemIfElse.c

---
+++ gradingSystemIfElse.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main()
+
+
+{
+
+int val1;
+int val2;
+int val3;
+
+int sum;
+
+printf("Enter your English marks: ");
+scanf("%d",&val1);
+
+printf("Enter your Maths marks: ");
+scanf("%d",&val2);
+
+printf("Enter your Physics marks: ");
+scanf("%d",&val3);
+
+sum = val1 + val2 + val3;
+
+if(sum < 100) // if(statement)
+{
+	printf("Failed");
+	
+}
+
+else if(sum == 300)// else if(statement)
+
+{
+	printf(" Your grade is A+");
+	
+}
+else if(sum > 300) 
+{
+	printf("invalid,,marks cannot exceed 300");
+}
+	
+
+else if(sum >= 250)// else if(statement)
+
+{
+	printf(" Your grade is A");
+	
+}
+
+else if(sum >= 150)// else if(statement)
+
+{
+	printf(" Your grade is B");
+	
+}
+
+else if(sum >= 100)// else if(statement)
+
+{
+	printf("Your grade is c ");
+	
+}
+
+	
+}
