27d46dd90ff6f5717550f5a2b70169f9f1d030b9
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-18 02:46:27 +0500

    Update printTableNestedLoop.c

--- printTableNestedLoop.c
+++ printTableNestedLoop.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int number = 6; // the number for which we want to print the table
+    int s = 6; // the number for which we want to print the table
 
     // outer loop (optional if you want multiple tables later)
     for (int i = 1; i <= 1; i++) {
         // inner loop to print the multiplication table of 60
         for (int j = 1; j <= 10; j++) {
-            printf("%d x %d = %d\n", number, j, number * j);
+            printf("%d x %d = %d\n", s, j, s * j);
         }
     }
 
