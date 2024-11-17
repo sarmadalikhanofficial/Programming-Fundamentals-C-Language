9e1f02044c18b527f9fe79138180b6e539ac9c1e
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-18 03:00:26 +0500

    Update gradingSystemIfElse.c

--- gradingSystemIfElse.c
+++ gradingSystemIfElse.c
@@ -4,62 +4,62 @@ int main()
 
 
 {
-
-int val1;
-int val2;
-int val3;
-
-int sum;
-
-printf("Enter your English marks: ");
-scanf("%d",&val1);
-
-printf("Enter your Maths marks: ");
-scanf("%d",&val2);
-
-printf("Enter your Physics marks: ");
-scanf("%d",&val3);
-
-sum = val1 + val2 + val3;
-
-if(sum < 100) // if(statement)
-{
-	printf("Failed");
 	
-}
-
-else if(sum == 300)// else if(statement)
-
-{
-	printf(" Your grade is A+");
+	int val1;
+	int val2;
+	int val3;
 	
-}
-else if(sum > 300) 
-{
-	printf("invalid,,marks cannot exceed 300");
-}
+	int sum;
 	
-
-else if(sum >= 250)// else if(statement)
-
-{
-	printf(" Your grade is A");
+	printf("Enter your English marks: ");
+	scanf("%d",&val1);
 	
-}
-
-else if(sum >= 150)// else if(statement)
-
-{
-	printf(" Your grade is B");
+	printf("Enter your Maths marks: ");
+	scanf("%d",&val2);
 	
-}
-
-else if(sum >= 100)// else if(statement)
-
-{
-	printf("Your grade is c ");
+	printf("Enter your Physics marks: ");
+	scanf("%d",&val3);
 	
-}
+	sum = val1 + val2 + val3;
+	
+	if(sum < 100) // if(statement)
+	{
+		printf("Failed");
+		
+	}
+	
+	else if(sum == 300)// else if(statement)
+	
+	{
+		printf(" Your grade is A+");
+		
+	}
+	else if(sum > 300) 
+	{
+		printf("invalid,,marks cannot exceed 300");
+	}
+		
+	
+	else if(sum >= 250)// else if(statement)
+	
+	{
+		printf(" Your grade is A");
+		
+	}
+	
+	else if(sum >= 150)// else if(statement)
+	
+	{
+		printf(" Your grade is B");
+		
+	}
+	
+	else if(sum >= 100)// else if(statement)
+	
+	{
+		printf("Your grade is c ");
+		
+	}
 
 	
 }
