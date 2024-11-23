e2019e4fed5fc3e331db2f46ec598180e47ba3af
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-23 17:24:40 +0500

    Create assignmentOperator.c

---
+++ assignmentOperator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+
+    printf("Value of a is %d\n", a);
+
+    a += 10;
+    printf("Value of a is %d\n", a);
+
+    a -= 10;
+    printf("Value of a is %d\n", a);
+
+    a *= 10;
+    printf("Value of a is %d\n", a);
+
+    a /= 10;
+    printf("Value of a is %d\n", a);
+
+    return 0;
+}
