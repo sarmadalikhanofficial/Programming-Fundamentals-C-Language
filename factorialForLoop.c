ba9340cbd6e038d7bedfca6b20200a450a5597cd
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-23 10:41:41 +0500

    Create factorialForLoop.c

---
+++ factorialForLoop.c
@@ -0,0 +1,24 @@
+//Make the program that calculates the factorial of the user given number .
+
+
+
+
+#include <stdio.h>  // Include the standard input/output header file.
+
+
+int main(){
+  int i, f = 1, num;  // Declare variables 'i' for loop counter, 'f' to store factorial, 'num' for user input.
+
+
+  printf("Input the number : ");  // Print a message to prompt user input.
+  scanf("%d", &num);  // Read the value of 'num' from the user.
+
+
+  for(i = 1; i <= num; i++)  // Start a loop to calculate factorial.
+      f = f * i;  // Calculate factorial.
+
+
+  printf("The Factorial of %d is: %d\n", num, f);  // Print the calculated factorial.
+}
+
+
