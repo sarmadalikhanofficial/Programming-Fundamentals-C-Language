a45e26621f814150145e2533cb17b8ac98273a85
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-23 10:37:20 +0500

    Create calculatetheAreaofaCircle.c

---
+++ calculatetheAreaofaCircle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    double radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);
+
+    area = PI * radius * radius;
+
+    printf("The area of the circle is: %.2lf\n", area);
+
+    return 0;
+}
