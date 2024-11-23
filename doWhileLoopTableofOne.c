3bb1b423c890f61c4849561161343d15d5c351a1
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-23 10:43:37 +0500

    Create doWhileLoopTableofOne.c

---
+++ doWhileLoopTableofOne.c
@@ -0,0 +1,13 @@
+// do while example
+// There is given the simple program of c language do while loop where we are printing the table of 1.
+
+
+#include<stdio.h>  
+int main(){    
+int i=1;      
+do{    
+printf("%d \n",i);    
+i++;    
+}while(i<=10);  
+return 0;  
+}
