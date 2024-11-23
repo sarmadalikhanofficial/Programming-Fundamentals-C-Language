844e512c3ad1d01abbe304f60ac94d18a12ca1ab
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-23 10:42:23 +0500

    Create whileLoopTableofOne.c

---
+++ whileLoopTableofOne.c
@@ -0,0 +1,15 @@
+// Example of the while loop in C language
+// Let's see the simple program of while loop that prints table of 1.
+
+
+#include<stdio.h>  
+int main()
+{    
+    int i=1;      
+    while(i<=10)
+{      
+    printf("%d \n",i);      
+    i++;      
+    }  
+return 0;  
+}
