57a2de616832194246b5d40d68ab17c217479187
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2025-01-01 03:12:40 +0500

    Create stringLenthPrint.c

---
+++ stringLenthPrint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Programming";
+
+    printf("The length of the string %s is: %d\n", str, strlen(str));
+
+    return 0;
+}
