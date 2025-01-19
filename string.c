7e25eab889093314fce6ea111285e218fa859f0c
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2025-01-19 14:33:57 +0500

    Create string.c

---
+++ string.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    
+    char string1 [] = "Sxrmd";    
+    char string2 [] = "Ali";    
+    
+//    strlwr(string1); //converts a string to lowercase
+//    strupr(string1); //converts a string to uppercase
+//    strcat(string1, string2); // concatenates the destination string and the source string
+//    strncat(string1, string2, 1), //appends n characters from string2 to string1
+//    strcpy(string1, string2); //copy string2 to string1
+//	  strncpy(string1, string2, 1); //copy n characters of string2 to stringl
+//	  strset(string1, '?');	//sets all the characters of a string to a given character
+//    strnset(string1, 'x' , 1);	//sets first n characters of a string to a given character
+//	  strrev(string1);	//reverses the string
+
+
+//	  printf("%s", string1);
+	
+	
+//	int result = strlen(string1);				// returns string length as int
+//	int result = strcmp(string1, string2);		// string compare all characters //use if else
+//	int result = strncmp(string1, string2, 1);	// string compare n characters
+//	int result = strcmpi(string1, string2);		// string compare all (ignore case)
+//	int result = strnicmp(string1, string2, 1);	// string compare n characters (ignore case)
+ 
+// string compare all (ignore case)
+// string compare n characters (ignore
+	
+
+
+//	printf("%d", result);
+
+
+//for this: int result = strcmp(string1, string2);
+//	if(result == 0)
+//	{
+//		printf("these strings are the same");
+//	}
+//	else
+//		printf("these strings are not the same");
+
+
+    return 0;
+}
+
+
