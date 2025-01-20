3c1d4dd9d56c6ce4448bf3d8921a891cea637234
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2025-01-20 09:34:50 +0500

    Create arrayInsertionSort.c

---
+++ arrayInsertionSort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int N) {
+    for (int i = 1; i < N; i++) {
+        int key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted: ");
+    for (int i = 0; i < N; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+    insertionSort(arr, N);
+
+    printf("Sorted: ");
+    for (int i = 0; i < N; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
